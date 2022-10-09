e24376d905f0fb8b7fb6db278fcce849613d3574
Johnny <knowyourlines@outlook.com>
2022-10-09 16:38:20 +0100

    enable anon user upgrade to phone user

--- src/components/PhoneOnlySignIn.vue
+++ src/components/PhoneOnlySignIn.vue
@@ -29,33 +29,31 @@ export default {
       new firebaseui.auth.AuthUI(firebase.auth());
     this.uiConfig = {
       signInSuccessUrl: window.location.href,
-    //   autoUpgradeAnonymousUsers: true,
+      autoUpgradeAnonymousUsers: true,
       signInOptions: [
         {
           provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
           defaultCountry: "GB",
         },
       ],
-    //   callbacks: {
-    //     // signInFailure callback must be provided to handle merge conflicts which
-    //     // occur when an existing credential is linked to an anonymous user.
-    //     signInFailure: function (error) {
-    //       // For merge conflicts, the error.code will be
-    //       // 'firebaseui/anonymous-upgrade-merge-conflict'.
-    //       if (error.code != "firebaseui/anonymous-upgrade-merge-conflict") {
-    //         console.log("hello word");
-    //         return Promise.resolve();
-    //       }
-    //       // The credential the user tried to sign in with.
-    //       var cred = error.credential;
-    //       // Copy data from anonymous user to permanent user and delete anonymous
-    //       // user at this stage if required.
-    //       // ...
-    //       // Finish sign-in after data is copied.
-    //       console.log("goodbye world");
-    //       return firebase.auth().signInWithCredential(cred);
-    //     },
-    //   },
+      callbacks: {
+        // signInFailure callback must be provided to handle merge conflicts which
+        // occur when an existing credential is linked to an anonymous user.
+        signInFailure: function (error) {
+          // For merge conflicts, the error.code will be
+          // 'firebaseui/anonymous-upgrade-merge-conflict'.
+          if (error.code != "firebaseui/anonymous-upgrade-merge-conflict") {
+            return Promise.resolve();
+          }
+          // The credential the user tried to sign in with.
+          var cred = error.credential;
+          // Copy data from anonymous user to permanent user and delete anonymous
+          // user at this stage if required.
+          // ...
+          // Finish sign-in after data is copied.
+          return firebase.auth().signInWithCredential(cred);
+        },
+      },
     };
     this.ui.start(this.$refs.firebaseui, this.uiConfig);
     firebase.auth().onAuthStateChanged((user) => {
