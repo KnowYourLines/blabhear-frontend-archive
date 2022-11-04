f740e339f8d868141ab3cea6ae8153b3e2cdffe9
Johnny <knowyourlines@outlook.com>
2022-11-04 00:17:00 +0000

    add space between name and message for notification

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -42,9 +42,8 @@
             <strong> {{ notification.room__display_name }}</strong>
             <br />
             <div v-if="notification.message__content">
-              {{ notification.message__creator__display_name }}:{{
-                notification.message__content
-              }}
+              {{ notification.message__creator__display_name }}:
+              {{ notification.message__content }}
             </div>
             <div v-else><br /></div>
             {{ notification.timestamp }} <br />
